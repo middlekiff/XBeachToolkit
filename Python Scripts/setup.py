74d0a58be41175193b6934a03b586ed7874e1288
middlekiff <middlekiff161@gmail.com>
2025-07-11 10:44:27 -0500

    Add files via upload

---
+++ Python Scripts/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 11 10:10:45 2025
+
+@author: middl
+"""
+
+!conda install pandas
+!conda install matplotlib
+!conda install numpy
+!conda install scipy
+!conda install configparser
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import griddata
+import configparser
+from scipy.interpolate import RegularGridInterpolator
+import os
+from scipy.interpolate import interp1d
+import subprocess
